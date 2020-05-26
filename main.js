6507daf4c8b43135b583a43ad9053e2c80542c05
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2020-05-26 14:37:44 +0545

    error validation added

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
-const button  = document.querySelector('button')
-const popup  =  document.getElementById('errors')
+const button = document.getElementById('btn');
+const error = document.getElementById('error');
+const message = document.getElementById('message');
+const input = document.getElementById('text');
 
-button.addEventListener('click',() => {
-  popup.style.display='block';
-});
+
+
+button.addEventListener('click', alertMessage)
+
+
+function alertMessage(){
+  if(input.value == "") {
+    error.style.display = "block";
+    message.style.display = "block"
+  }
+  else {
+   error.style.display = "none";
+  }
+}
